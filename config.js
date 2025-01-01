b38a0ca109d6dd8b883e53d79d69ae6725a5e408
dev-bhaskar8 <vaas.ygg@gmail.com>
2025-01-01 16:46:48 +0530

    Revert to b17c69c96d04cbada89a2771761a96b02c53b901

--- config.js
+++ config.js
@@ -3,7 +3,5 @@ const config = {
     AMAZON_ASSOCIATE_ID: 'bhaskar0d0-21'  // Your Amazon Associate ID
 };
 
-// Store config in Chrome storage
-chrome.storage.local.set({ appConfig: config }, function() {
-    console.log('Configuration saved to storage');
-});
+// Make config available globally
+window.appConfig = config;
